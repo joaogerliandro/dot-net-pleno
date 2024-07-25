e8e095738797b5423d84227e324c1a8d9bdb5f9f
João Gerliandro <joaogerliandro@gmail.com>
2024-07-25 10:54:59 -0300

    feat: add entities mapings

---
+++ StallosDotnetPleno.Infrastructure/Mappings/PersonMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StallosDotnetPleno.Domain.Entities;
+
+namespace StallosDotnetPleno.Infrastructure.Mappings
+{
+    public class PersonMap : BaseEntityMap<Person>
+    {
+        public override void Configure(EntityTypeBuilder<Person> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(p => p.Type)
+                .IsRequired();
+
+            builder.Property(p => p.Document)
+                .IsRequired()
+                .HasMaxLength(255);
+        }
+    }
+}
