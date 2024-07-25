1fb9e1225957568370323a175b1b5bb6ff1fb120
João Gerliandro <joaogerliandro@gmail.com>
2024-07-25 10:07:02 -0300

    feat: add entities context classes

---
+++ StallosDotnetPleno.Infrastructure/Context/AddressContext.cs
@@ -0,0 +1,19 @@
+﻿using StallosDotnetPleno.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace StallosDotnetPleno.Infrastructure.Context
+{
+    public class AddressContext : DbContext
+    {
+        public AddressContext(DbContextOptions<AddressContext> options) : base(options) { }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AddressContext).Assembly);
+        }
+    }
+}
