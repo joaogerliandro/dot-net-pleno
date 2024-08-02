dca985a2670d0563cb52390ea6c2e4efd5b1c0b0
João Gerliandro <joaogerliandro@gmail.com>
2024-08-02 16:01:07 -0300

    feat: add api protocol getter

---
+++ StallosDotnetPleno.Application/ResultObjects/ProtocolResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StallosDotnetPleno.Application.ResultObjects
+{
+    public class ProtocolResult
+    {
+        [JsonPropertyName("Protocolo")]
+        public string Protocol { get; set; }
+        public string Message { get; set; }
+    }
+}
--- StallosDotnetPleno.Application/Services/RosterServices/RosterApiService.cs
+++ StallosDotnetPleno.Application/Services/RosterServices/RosterApiService.cs
@@ -1,17 +1,22 @@
 ﻿using StallosDotnetPleno.Application.Helpers;
 using StallosDotnetPleno.Application.Interfaces;
+using StallosDotnetPleno.Application.ResultObjects;
 using StallosDotnetPleno.Domain.Entities;
 using StallosDotnetPleno.Domain.Enums;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace StallosDotnetPleno.Application.Services.RosterServices
 {
     public class RosterApiService : IRosterApiService
     {
         private readonly ConfigHelper _configHelper;
+        private readonly Dictionary<string, string> _protocolCache;
 
         public RosterApiService(ConfigHelper configHelper)
         {
             _configHelper = configHelper;
+            _protocolCache = new Dictionary<string, string>();
         }
 
         public async Task<ICollection<PublicList>> ConsultPersonPublicList(Person person)
@@ -50,11 +55,12 @@ namespace StallosDotnetPleno.Application.Services.RosterServices
 
         private async Task<bool> ConsultList(string listName, Person person)
         {
-            var protocol = "protocol"; // Get Protocol
             var token = "token"; // Get Token
 
+            var protocol = await GetProtocol(listName, token);
+            var requestUri = await PrepareRequestUri(_configHelper.RosterApiHostname, listName, person);
+
             var client = new HttpClient();
-            var requestUri = PrepareRequestUri(_configHelper.RosterApiHostname, listName, person);
             var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
 
             request.Headers.Add("accept", "application/json");
@@ -74,7 +80,7 @@ namespace StallosDotnetPleno.Application.Services.RosterServices
             }
         }
 
-        private static string PrepareRequestUri(string url, string listName, Person person)
+        private async Task<string> PrepareRequestUri(string url, string listName, Person person)
         {
             string uri = url + listName + "?nome=" + Uri.EscapeDataString(person.Name);
 
@@ -82,6 +88,60 @@ namespace StallosDotnetPleno.Application.Services.RosterServices
                 uri += "&cpf=" + Uri.EscapeDataString(person.Document);
 
             return uri;
+        }
+
+        private async Task<string> GetProtocol(string listName, string token)
+        {
+            if (!(_protocolCache.TryGetValue(listName, out var protocol)) || String.IsNullOrEmpty(protocol)) // If not exists in the cache, create a new one
+            {
+                protocol = await CreateProtocol(listName, token); 
+                
+                _protocolCache[listName] = protocol; // Add protocol to cache
+            }
+
+            return protocol;
+        }
+
+        private async Task<string> CreateProtocol(string listName, string token)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, _configHelper.RosterApiHostname + "protocolo");
+
+            request.Headers.Add("accept", "application/json");
+            request.Headers.Add("x-api-key", _configHelper.RosterXApi);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var requestBody = new
+            {
+                Responsavel = "XPTO",
+                Origem = 2,
+                Consulta = new
+                {
+                    Nome = "XPTO",
+                    Documento = "00000000000"
+                },
+                Listas = new string[] { listName }
+            };
+
+            request.Content = new StringContent(
+                JsonSerializer.Serialize(requestBody),
+                System.Text.Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var protocolResult = JsonSerializer.Deserialize<ProtocolResult>(responseBody);
+
+                return String.IsNullOrEmpty(protocolResult.Protocol) ? string.Empty : protocolResult.Protocol;
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
     }
 }
